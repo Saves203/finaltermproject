cef96349fa2d7ec6792688b9fb53dfc0156d7347
louisnguyn_ <91862286+Saves203@users.noreply.github.com>
2024-06-12 11:41:00 +0700

    Delete main directory

--- main/light.cpp
+++
@@ -1,8 +0,0 @@
-#include "light.h"
-
-static const char *TAG = "example";
-
-void configure_led(gpio_num_t pin) {
-    ESP_LOGI(TAG, "Example configured to blink GPIO LED!");
-    gpio_set_direction(pin, GPIO_MODE_OUTPUT);
-}
--- main/light.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LIGHT_H_
-#define _LIGHT_H_
-
-#include "esp_log.h"
-#include "driver/gpio.h"
-
-void configure_led(gpio_num_t pin);
-
-#endif
